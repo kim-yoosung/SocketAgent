30e1c66da8db414a2084263d098ace1e873e161f
김유성 <helikim1127@lguplus.co.kr>
2025-04-17 17:18:43 +0900

    feat : 변경사항 저장

--- src/main/java/com/example/GetOutputStreamAdvice.java
+++ src/main/java/com/example/GetOutputStreamAdvice.java
@@ -1,26 +1,14 @@
 package com.example;
 
 import net.bytebuddy.asm.Advice;
-
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 
 public class GetOutputStreamAdvice {
     @Advice.OnMethodExit
     public static void onExit(@Advice.Return(readOnly = false) OutputStream returned) {
-        System.out.println("[Agent] getOutputStream() 호출 → 무력화된 OutputStream으로 대체");
-
-        returned = new OutputStream() {
-            @Override
-            public void write(int b) {
-                // 아무것도 안함
-                System.out.println("[Agent] write() → 차단됨");
-            }
-
-            @Override
-            public void write(byte[] b, int off, int len) {
-                // 무시
-                System.out.println("[Agent] write(byte[]) → 전송 차단됨");
-            }
-        };
+        System.out.println("[Agent] getOutputStream() 호출됨");
+        // ByteArrayOutputStream을 사용하여 데이터를 메모리에 저장
+        returned = new ByteArrayOutputStream();
     }
 }
