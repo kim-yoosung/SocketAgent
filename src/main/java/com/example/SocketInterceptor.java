bcb0c904a5fabbab5a80387357af1e3a5d2555bc
김유성 <helikim1127@lguplus.co.kr>
2025-04-21 15:27:58 +0900

    feat : ip 로깅 성공

--- src/main/java/com/example/SocketInterceptor.java
+++ src/main/java/com/example/SocketInterceptor.java
@@ -9,6 +9,8 @@ public class SocketInterceptor {
     @Advice.OnMethodEnter
     public static void onEnter(@Advice.Origin Method method,
                                @Advice.AllArguments Object[] args) {
+        Object endpoint = args[0];String raw = endpoint.toString();
+        System.out.println("!!!!!!!" + raw);
         System.out.println("[Agent] " + method.getName() + "() 호출됨");
         if (args != null && args.length > 0) {
             for (Object arg : args) {
