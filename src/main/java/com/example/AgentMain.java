4e6627d23505bc415ce8f1b848f32160a5cf68c3
김유성 <helikim1127@lguplus.co.kr>
2025-04-21 15:52:05 +0900

    feat : ip 로깅 리스트로 관리

--- src/main/java/com/example/AgentMain.java
+++ src/main/java/com/example/AgentMain.java
@@ -2,12 +2,8 @@ package com.example;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.utility.JavaModule;
 
 import java.lang.instrument.Instrumentation;
-import java.net.Socket;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
@@ -15,23 +11,8 @@ public class AgentMain {
     public static void premain(String agentArgs, Instrumentation inst) {
 
         System.out.println("[agent] gogo");
-        AgentBuilder.Listener listener = new AgentBuilder.Listener.Adapter() {
-            @Override
-            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader,
-                                         JavaModule module, boolean loaded, DynamicType dynamicType) {
-                System.out.println("[Agent] Transformed: " + typeDescription.getName());
-            }
-
-            @Override
-            public void onError(String typeName, ClassLoader classLoader,
-                                JavaModule module, boolean loaded, Throwable throwable) {
-                System.out.println("[Agent] Error: " + typeName);
-                throwable.printStackTrace();
-            }
-        };
 
         new AgentBuilder.Default()
-                .with(listener)
                 .ignore(none())
                 .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
                 .type(named("java.net.Socket"))
--- src/main/java/com/example/SocketInterceptor.java
+++ src/main/java/com/example/SocketInterceptor.java
@@ -3,6 +3,7 @@ package com.example;
 import net.bytebuddy.asm.Advice;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class SocketInterceptor {
 
@@ -10,9 +11,22 @@ public class SocketInterceptor {
     public static void onEnter(@Advice.Origin Method method,
                                @Advice.AllArguments Object[] args) {
         Object endpoint = args[0];String raw = endpoint.toString();
-        System.out.println("!!!!!!!" + raw);
         System.out.println("[Agent] " + method.getName() + "() 호출됨");
-        if (args != null && args.length > 0) {
+        String ip = "";
+        int slash = raw.indexOf('/');
+        int colon = raw.indexOf(':');
+        if (slash >= 0 && colon > slash) {
+            ip = raw.substring(slash + 1, colon);
+        }
+        System.out.println("[Agent] " + ip + "ip 호출됨");
+
+        String[] interceptIpArray = {
+                "192.168.0.10",
+                "10.0.0.5",
+                "142.250.66.46"
+        };
+
+        if (args != null && args.length > 0 && Arrays.asList(interceptIpArray).contains(ip)) {
             for (Object arg : args) {
                 System.out.println("         └ 파라미터: " + arg);
             }
